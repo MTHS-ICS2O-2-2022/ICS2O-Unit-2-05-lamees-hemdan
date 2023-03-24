6f0b9acd62c65cc1e8ad4de46c19361b4d31d007
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-24 17:31:06 +0000

    eddited js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
+'use strict'
+/**
+ * This function calculates your income tax
+ */
   function myButtonClicked() {
-    // This program calculates salary with tax
-    var 
+    // input
+    const hours = parseFloat(document.getElementById('hours').value)
+    const salary = parseFloat(document.getElementById('salary').value)
+
+    // process
+    const pay = (hours * salary) * (1 - 0.18)
+    const governmentPay = (hours * salary) * 0.18
+    
+    // output
+    document.getElementById('pay').innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
+    document.getElementById('tax').innerHTML = `Government will Take: $ ${governmentPay.toFixed(2)}`
   }
